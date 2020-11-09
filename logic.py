c4904daf137a8406b639b04881f371f17bf96a13
Gino Galotti <gino.galotti@gmail.com>
2020-11-09 19:49:50 +0100

    Add decimal handling

--- logic.py
+++ logic.py
@@ -1,8 +1,10 @@
 def format_number(value):
+
+    # We discard non-numbers
     try:
         value = float(value)
     except ValueError:
         return "Sorry, I need a numerical value"
 
-
-    return '{}0'.format(value) 
+    # Now we deal with the decimal parts
+    return '%.2f' % value 
--- test_logic.py
+++ test_logic.py
@@ -4,17 +4,24 @@ from logic import format_number
 
 NO_NUMBER_ERROR = "Sorry, I need a numerical value"
 
-def test_0():
-    assert "0.00" == format_number(0)
-
-def test_2_digits():
-    assert "22.00" == format_number(22)
-
 def test_no_decimals():
     assert "1.00" == format_number(1)
+    assert "22.00" == format_number(22)
+    assert "0.00" == format_number(0)
     
 def test_not_a_number():
     assert NO_NUMBER_ERROR == format_number("a")
     assert NO_NUMBER_ERROR == format_number("11.1a")
     assert NO_NUMBER_ERROR == format_number("234asbdweqefewr.dsadwqewqe")
     assert NO_NUMBER_ERROR == format_number("1234125.O22222")
+
+def test_filling_decimals():
+    assert "1.10" == format_number(1.1)
+    assert "12313.90" == format_number(12313.9)
+    assert "12313.00" == format_number(12313.0)
+
+def test_rounding_decimals():
+    assert "1.10" == format_number(1.099)
+    assert "12313.03" == format_number(12313.034)
+    assert "12313.01" == format_number(12313.009)
+

47579924f6da5c65208951c12a1f20e6da1e4655
Gino Galotti <gino.galotti@gmail.com>
2020-11-09 19:42:19 +0100

    Added logic to only parse numbers

--- logic.py
+++ logic.py
@@ -1,2 +1,8 @@
 def format_number(value):
-    return '{}.00'.format(value) 
+    try:
+        value = float(value)
+    except ValueError:
+        return "Sorry, I need a numerical value"
+
+
+    return '{}0'.format(value) 
--- test_logic.py
+++ test_logic.py
@@ -2,6 +2,8 @@ import pytest
 
 from logic import format_number
 
+NO_NUMBER_ERROR = "Sorry, I need a numerical value"
+
 def test_0():
     assert "0.00" == format_number(0)
 
@@ -10,3 +12,9 @@ def test_2_digits():
 
 def test_no_decimals():
     assert "1.00" == format_number(1)
+    
+def test_not_a_number():
+    assert NO_NUMBER_ERROR == format_number("a")
+    assert NO_NUMBER_ERROR == format_number("11.1a")
+    assert NO_NUMBER_ERROR == format_number("234asbdweqefewr.dsadwqewqe")
+    assert NO_NUMBER_ERROR == format_number("1234125.O22222")
